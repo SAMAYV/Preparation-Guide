054d5da9ec4cfdb7fe2cadea45e3c97b7cbd423e
SAMAYV <samay@iitg.ac.in>
2022-02-10 22:34:50 +0530

    move constructor

--- Threads and Locks/move_constructor.cpp
+++ Threads and Locks/move_constructor.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Container 
 {
-    int * m_Data;
+    int* m_Data;
 public:
     Container(){
         // Allocate an array of 20 int on heap
@@ -18,7 +18,8 @@ public:
         }
     }
     // Copy Constructor
-    Container(const Container& obj){
+    Container(const Container& obj)
+    {
         // Allocate an array of 20 int on heap
         m_Data = new int[20];
         // Copy the data from passed object
@@ -28,7 +29,8 @@ public:
         cout << "Copy Constructor: Allocation 20 int" << endl;
     }
     // Assignment Operator
-    Container &operator=(const Container& obj){
+    Container& operator=(const Container& obj)
+    {
         if(this != &obj)
         {
             // Allocate an array of 20 int on heap
@@ -64,7 +66,7 @@ public:
         return *this;
     }
 };
-// Create am object of Container and return
+// Create an object of Container and return
 Container getContainer()
 {
     Container obj;
@@ -74,9 +76,22 @@ int main()
 {
     // Create a vector of Container Type
     vector<Container> vecOfContainers;
+
     // Add object returned by function into the vector
     vecOfContainers.push_back(getContainer());
     Container obj;
     obj = getContainer();
     return 0;
 }
+
+/* In the move constructor, we just copied the pointer. Now member variable m_Data points to the same memory on heap. 
+Then we set the m_Data of passed object to NULL. So, we didn’t allocated any memory on heap in move constructor, we just 
+shifted the control of memory. Now if we create the vector  of class container and push a object returned from getContainer() 
+into it. Then a new object will created from this temporary object but as getContainer() is a rvalue, so Move Constructor 
+of this new Container class’s object will be called and in that memory will be just shifted. So, actually on heap we will 
+create only one array of integers. */
+
+/* Generally, we use Factory classes to create object of our classes. But we actually created 2 objects for it because 
+getContainer() function returned a temporary object which got copied into a new object and then destructed. 
+This 2nd object got inserted in vector. How to solve this problem of resource and effort wastage due to temporary objects? 
+Is there a way to move the 1st object instead of creating 2nd one and copying contents to it? */
